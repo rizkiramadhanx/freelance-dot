4f09adfc04347eb83612c3a1381343216bf75240
Rizki Ramadhan <rizkijitu77@gmail.com>
2024-03-28 20:40:55 +0700

    feat: Carousel with left and right arrow keys
    Added left and right arrow keys to the carousel to improve usability.
    
    BREAKING CHANGE: The carousel now uses a reference to access the slick slider instance. If you are using the carousel in your code, you will need to update your code to use the new reference.

--- src/components/page/main/Carousel.tsx
+++ src/components/page/main/Carousel.tsx
@@ -6,8 +6,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const settings: Settings = {
-  dots: true,
-  arrows: true,
+  dots: false,
+  arrows: false,
   infinite: true,
   speed: 500,
   slidesToShow: 1,
@@ -15,40 +15,73 @@ const settings: Settings = {
 }
 
 const Carousel = () => {
-  return (
-    <div className='flex justify-center'>
-      <div className='container'>
-        <Slider {...settings}>
-          <div className='bg-red-500 max-h-[500px]'>
-            <Image
-              width={500}
-              height={500}
-              src='/img/scandinavia.png'
-              className=' w-full'
-              alt='carousel image'
-            />
-          </div>
-          <div className='bg-red-500 max-h-[500px]'>
-            <Image
-              width={500}
-              height={500}
-              src='/img/scandinavia.png'
-              className='w-full'
-              alt='carousel image'
-            />
-          </div>
-          <div className='bg-red-500 max-h-[500px]'>
 
+  const sliderRef = React.useRef<Slider | null>(null);
+
+  const gotoNext = () => {
+    sliderRef.current?.slickNext();
+  };
+
+  const gotoPrev = () => {
+    sliderRef.current?.slickPrev();
+  };
+  return (
+    <div className='relative'>
+      <div className='w-full z-[99] top-0.5  h-full absolute px-3 flex justify-between'>
+        <div className='flex h-full items-center'
+          onClick={() => gotoPrev()}
+        >
+          <div className='md:p-4 p-2 bg-white border-[1px] border-[#F5333F]'>
             <Image
-              width={500}
-              src='/img/scandinavia.png'
-              height={500}
-              className=' w-full'
-              alt='carousel image'
+              alt='left'
+              height={12}
+              width={12}
+              src='/svg/left.svg'
             />
           </div>
-        </Slider>
+        </div>
+        <div className='flex h-full items-center'
+          onClick={() => gotoNext()}
+        >
+          <div className='md:p-4 p-2 bg-white border-[1px] border-[#F5333F]'><Image
+            alt='left'
+            height={12}
+            width={12}
+            src='/svg/right.svg'
+          /></div>
+        </div>
+
       </div>
+      <Slider {...settings} ref={sliderRef}>
+        <div className='max-h-[500px]'>
+          <Image
+            width={500}
+            height={500}
+            src='/img/scandinavia.png'
+            className=' w-full'
+            alt='carousel image'
+          />
+        </div>
+        <div className='max-h-[500px]'>
+          <Image
+            width={500}
+            height={500}
+            src='/img/scandinavia.png'
+            className='w-full'
+            alt='carousel image'
+          />
+        </div>
+        <div className='max-h-[500px]'>
+
+          <Image
+            width={500}
+            src='/img/scandinavia.png'
+            height={500}
+            className=' w-full'
+            alt='carousel image'
+          />
+        </div>
+      </Slider>
     </div>
   )
 }
