7000ceaeaf10e7c1b25d8ad066383a4c9e4788ad
Rizki Ramadhan <rizkijitu77@gmail.com>
2024-03-28 21:01:02 +0700

    refactor: Refactor Navbar component
    This commit refactors the Navbar component to improve the user experience and responsiveness.
    
    - Adds a toggle button to the mobile navigation to display the navigation links.
    - Moves the navigation links to a flex column layout on mobile to improve readability and usability.
    - Updates the styling to match the rest of the application.

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -6,7 +6,8 @@ import { twJoin } from 'tailwind-merge'
 const Navbar = () => {
 
   const [onTop, setOnTop] = useState(false)
-  
+  const [toggle,setToggle] = useState(false)
+
   const handleScroll = () => {
     if (onTop !== (window.pageYOffset === 0)) {
       setOnTop(window.pageYOffset === 0);
@@ -21,7 +22,7 @@ const Navbar = () => {
   });
 
   return (
-    <div className={twJoin("md:py-10 flex justify-center fixed z-[100] bg-white py-5 w-full",!onTop && "shadow-md transition-all") }>
+    <div className={twJoin("md:py-10 flex justify-center fixed z-[100] bg-white py-5 w-full", !onTop && "shadow-md transition-all")}>
       <div className="container ">
         <div className="flex justify-between items-center">
           <Image
@@ -32,16 +33,16 @@ const Navbar = () => {
             className="h-[30px] md:h-[42px] w-auto"
           />
           <div className="font-semibold md:flex gap-8 items-center hidden">
-            <div className="flex gap-[13px] items-center">
+            <div className="flex gap-[13px] items-center hover:text-red-brand cursor-pointer">
               <div>Tentang Kami</div>
               <DropdownIcon />
             </div>
-            <div className="flex gap-[13px] items-center">
+            <div className="flex gap-[13px] items-center hover:text-red-brand cursor-pointer">
               <div>Produk & Layanan</div>
               <DropdownIcon />
             </div>
-            <div>Blog</div>
-            <div>FAQ</div>
+            <div className=' hover:text-red-brand cursor-pointer'>Blog</div>
+            <div className=' hover:text-red-brand cursor-pointer'>FAQ</div>
           </div>
           <div className="md:flex hidden items-center gap-4 font-semibold">
             <button className="px-4 py-[10px] rounded-md">Daftar</button>
@@ -49,7 +50,7 @@ const Navbar = () => {
               Masuk
             </button>
           </div>
-          <div className='md:hidden'>
+          <div className='md:hidden' onClick={() =>setToggle(!toggle)}>
             <Image
               alt="hamburger"
               src="/svg/menu-humberger.svg"
@@ -57,6 +58,26 @@ const Navbar = () => {
               width={100}
               className="h-[30px] md:h-[42px] w-auto"
             />
+          </div>
+        </div>
+        <div className={twJoin('flex md:hidden flex-col justify-between h-screen', toggle && 'hidden')}>
+          <div className='mt-4'>
+            <div className="flex gap-[13px] justify-between items-center">
+              <div>Tentang Kami</div>
+              <DropdownIcon />
+            </div>
+            <div className="flex gap-[13px] justify-between items-center">
+              <div>Produk & Layanan</div>
+              <DropdownIcon />
+            </div>
+            <div>Blog</div>
+            <div>FAQ</div>
+          </div>
+          <div className="flex flex-col items-center mb-[100px] gap-2 font-semibold">
+            <button className="px-4 py-[10px] w-full rounded-md bg-slate-300">Daftar</button>
+            <button className="px-4 py-[10px] rounded-md w-full bg-red-brand text-white">
+              Masuk
+            </button>
           </div>
         </div>
       </div>
